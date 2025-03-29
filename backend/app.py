952c4444ffc25261fa83657b815f342abeb8be74
Kalyani Patra <71889838+Kalyani02072003@users.noreply.github.com>
2025-03-29 20:00:12 +0530

    add blueprint for endpoint

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from backend.routes.protein_mpnn import proteinmpnn_bp
 from backend.routes.tasks import tasks_bp
 from backend.routes.ligand_mpnn import ligandmpnn_bp
 from backend.routes.thompson_sampling import ts_bp
+from backend.routes.free_wilson import freewilson_bp
 app = Flask(__name__)
 
 
@@ -15,6 +16,7 @@ app.register_blueprint(proteinmpnn_bp, url_prefix="/v1/api/proteinmpnn")
 app.register_blueprint(tasks_bp, url_prefix="/v1/api/tasks")
 app.register_blueprint(ligandmpnn_bp, url_prefix="/v1/api/ligandmpnn")
 app.register_blueprint(ts_bp, url_prefix="/v1/api/thompson_sampling")
+app.register_blueprint(freewilson_bp, url_prefix="/v1/api/freewilson")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
